79dd8231656d14f7382c1407b1627dc483146d67
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:28:41 +0100

    Aggiunta 1.19.3-Forge

---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/OnlyHammersAndExcavators.java
@@ -0,0 +1,52 @@
+package com.blocklegend001.onlyhammersandexcavators;
+
+import com.blocklegend001.onlyhammersandexcavators.item.ModCreativeModeTabs;
+import com.blocklegend001.onlyhammersandexcavators.item.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(OnlyHammersAndExcavators.MOD_ID)
+public class OnlyHammersAndExcavators {
+    public static final String MOD_ID = "onlyhammersandexcavators";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public OnlyHammersAndExcavators() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(modEventBus);
+        modEventBus.addListener(this::addCreative);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event) {
+        if (event.getTab() == ModCreativeModeTabs.ONLYHAMMERSANDEXCAVATORS) {
+            event.accept(ModItems.WOODEN_EXCAVATOR.get());
+            event.accept(ModItems.STONE_EXCAVATOR.get());
+            event.accept(ModItems.IRON_EXCAVATOR.get());
+            event.accept(ModItems.GOLD_EXCAVATOR.get());
+            event.accept(ModItems.LAPIS_EXCAVATOR.get());
+            event.accept(ModItems.REDSTONE_EXCAVATOR.get());
+            event.accept(ModItems.DIAMOND_EXCAVATOR.get());
+            event.accept(ModItems.EMERALD_EXCAVATOR.get());
+            event.accept(ModItems.OBSIDIAN_EXCAVATOR.get());
+            event.accept(ModItems.NETHERITE_EXCAVATOR.get());
+
+            event.accept(ModItems.WOODEN_HAMMER.get());
+            event.accept(ModItems.STONE_HAMMER.get());
+            event.accept(ModItems.IRON_HAMMER.get());
+            event.accept(ModItems.GOLD_HAMMER.get());
+            event.accept(ModItems.LAPIS_HAMMER.get());
+            event.accept(ModItems.REDSTONE_HAMMER.get());
+            event.accept(ModItems.DIAMOND_HAMMER.get());
+            event.accept(ModItems.EMERALD_HAMMER.get());
+            event.accept(ModItems.OBSIDIAN_HAMMER.get());
+            event.accept(ModItems.NETHERITE_HAMMER.get());
+        }
+    }
+}
---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/datagen/DataGenerators.java
@@ -0,0 +1,21 @@
+package com.blocklegend001.onlyhammersandexcavators.datagen;
+
+import com.blocklegend001.onlyhammersandexcavators.OnlyHammersAndExcavators;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = OnlyHammersAndExcavators.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        PackOutput packOutput = event.getGenerator().getPackOutput();
+
+        generator.addProvider(true, new ModItemModelProvider(packOutput, existingFileHelper));
+    }
+}
