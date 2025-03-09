af658e084c2929d242db1bc6a80afad52a93a0bc
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:30:30 +0100

    Aggiunta 1.20-Fabric

---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/OnlyHammersAndExcavators.java
@@ -0,0 +1,18 @@
+package com.blocklegend001.onlyhammersandexcavators;
+
+import com.blocklegend001.onlyhammersandexcavators.item.ModItemGroup;
+import com.blocklegend001.onlyhammersandexcavators.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OnlyHammersAndExcavators implements ModInitializer {
+    public static final String MOD_ID = "onlyhammersandexcavators";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        ModItemGroup.registerItemGroups();
+        ModItems.registerModItems();
+    }
+}
---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/item/custom/excavator/IronExcavator.java
@@ -0,0 +1,10 @@
+package com.blocklegend001.onlyhammersandexcavators.item.custom.excavator;
+
+import com.blocklegend001.onlyhammersandexcavators.item.custom.Excavator;
+import net.minecraft.item.ToolMaterial;
+
+public class IronExcavator extends Excavator {
+    public IronExcavator(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
