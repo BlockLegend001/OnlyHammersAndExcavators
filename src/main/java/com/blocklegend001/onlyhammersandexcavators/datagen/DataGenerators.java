b22ef2240aa43fdab9920a43a5c7079fc618ce17
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:31:31 +0100

    Aggiunta 1.21.4-NeoForge

---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/datagen/DataGenerators.java
@@ -0,0 +1,30 @@
+package com.blocklegend001.onlyhammersandexcavators.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.blocklegend001.onlyhammersandexcavators.OnlyHammersAndExcavators.MODID;
+
+@EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent.Client event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        event.addProvider(new ModItemModelProvider(packOutput, existingFileHelper));
+        ModBlockTagGenerator blockTagGenerator = event.addProvider(
+                new ModBlockTagGenerator(packOutput, lookupProvider, existingFileHelper));
+        event.addProvider(new ModItemTagGenerator(packOutput, lookupProvider, blockTagGenerator.contentsGetter(), existingFileHelper));
+        event.addProvider(new ModRecipeProvider.Runner(packOutput, lookupProvider));
+    }
+}
