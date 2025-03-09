cca3bfba4c564d047681f0258617d6ec0b5a5ea2
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:30:41 +0100

    Aggiunta 1.20.4-Fabric

---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/item/custom/hammer/DiamondHammer.java
@@ -0,0 +1,11 @@
+package com.blocklegend001.onlyhammersandexcavators.item.custom.hammer;
+
+import com.blocklegend001.onlyhammersandexcavators.item.custom.Hammer;
+import net.minecraft.item.ToolMaterial;
+
+public class DiamondHammer extends Hammer {
+
+    public DiamondHammer(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/item/custom/hammer/IronHammer.java
@@ -0,0 +1,11 @@
+package com.blocklegend001.onlyhammersandexcavators.item.custom.hammer;
+
+import com.blocklegend001.onlyhammersandexcavators.item.custom.Hammer;
+import net.minecraft.item.ToolMaterial;
+
+public class IronHammer extends Hammer {
+
+    public IronHammer(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
