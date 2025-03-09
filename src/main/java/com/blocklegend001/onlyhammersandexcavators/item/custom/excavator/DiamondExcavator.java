dbeec9f45badf279a8d165c73585331ac824c445
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:30:37 +0100

    Aggiunta 1.20.2-Fabric

---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/item/custom/excavator/DiamondExcavator.java
@@ -0,0 +1,10 @@
+package com.blocklegend001.onlyhammersandexcavators.item.custom.excavator;
+
+import com.blocklegend001.onlyhammersandexcavators.item.custom.Excavator;
+import net.minecraft.item.ToolMaterial;
+
+public class DiamondExcavator extends Excavator {
+    public DiamondExcavator(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
