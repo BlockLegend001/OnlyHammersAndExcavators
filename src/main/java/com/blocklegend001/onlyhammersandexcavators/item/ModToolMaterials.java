c5b2c6d32d36fbeaa747a445b2960afc0dc51a6e
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-03 15:12:59 +0200

    Now you can break block in creative mode

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/item/ModToolMaterials.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/item/ModToolMaterials.java
@@ -207,7 +207,7 @@ public record ModToolMaterials(TagKey<Block> incorrectBlocksForDrops, int durabi
                         ),
                         1.0F,
                         1,
-                        false
+                        true
                 ))
                 .attributes(createToolAttributes(damage, speed));
     }
