4b3fe68dbbf60e356af327b2fa97b7a1b9f239e5
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-08 14:30:38 +0200

    If the player is sneaking (shift held), the Excavator will only break the single targeted block.

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/ExcavatorUsageEvent.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/ExcavatorUsageEvent.java
@@ -14,28 +14,38 @@ import org.jetbrains.annotations.Nullable;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ExcavatorUsageEvent implements PlayerBlockBreakEvents.Before{
+import static com.blocklegend001.onlyhammersandexcavators.item.custom.Excavator.getBlocksToBeDestroyed;
+
+public class ExcavatorUsageEvent implements PlayerBlockBreakEvents.Before {
     private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>();
+    public static boolean isSneaking = false;
 
     @Override
     public boolean beforeBlockBreak(World world, PlayerEntity player, BlockPos pos,
                                     BlockState state, @Nullable BlockEntity blockEntity) {
+        if (!(player instanceof ServerPlayerEntity serverPlayer)) return true;
+
         ItemStack mainHandItem = player.getMainHandStack();
+        if (!(mainHandItem.getItem() instanceof Excavator excavator)) return true;
 
-        if(mainHandItem.getItem() instanceof Excavator excavator && player instanceof ServerPlayerEntity serverPlayer) {
-            if(HARVESTED_BLOCKS.contains(pos)) {
-                return true;
+        if (HARVESTED_BLOCKS.contains(pos)) return true;
+
+        HARVESTED_BLOCKS.add(pos);
+
+        try {
+            int radius = isSneaking ? 0 : 1;
+            for (BlockPos targetPos : getBlocksToBeDestroyed(radius, pos, serverPlayer)) {
+                if (targetPos.equals(pos)) continue;
+
+                if (HARVESTED_BLOCKS.contains(targetPos)) continue;
+                if (!excavator.isCorrectForDrops(mainHandItem, world.getBlockState(targetPos))) continue;
+
+                HARVESTED_BLOCKS.add(targetPos);
+                serverPlayer.interactionManager.tryBreakBlock(targetPos);
+                HARVESTED_BLOCKS.remove(targetPos);
             }
-
-            for(BlockPos position : Excavator.getBlocksToBeDestroyed(1, pos, serverPlayer)) {
-                if(pos == position || !excavator.isCorrectForDrops(mainHandItem, world.getBlockState(position))) {
-                    continue;
-                }
-
-                HARVESTED_BLOCKS.add(position);
-                serverPlayer.interactionManager.tryBreakBlock(position);
-                HARVESTED_BLOCKS.remove(position);
-            }
+        } finally {
+            HARVESTED_BLOCKS.remove(pos);
         }
 
         return true;
