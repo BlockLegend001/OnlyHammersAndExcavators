6cda087ea1d4828eda6312a2ca4c1dced13dc8f7
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-08 14:31:55 +0200

    If the player is sneaking (shift held), the Excavator will only break the single targeted block.

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/ExcavatorOverlayRenderer.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/ExcavatorOverlayRenderer.java
@@ -18,6 +18,7 @@ import net.minecraft.util.math.Vec3d;
 import org.joml.Matrix4f;
 
 public class ExcavatorOverlayRenderer {
+
     public static void init() {
         WorldRenderEvents.AFTER_ENTITIES.register(context -> {
             MinecraftClient client = MinecraftClient.getInstance();
@@ -30,7 +31,12 @@ public class ExcavatorOverlayRenderer {
 
             Direction side = blockHit.getSide();
             BlockPos origin = blockHit.getBlockPos();
-            int range = 1;
+            int range;
+            if (client.player.isSneaking()) {
+                range = 0;
+            } else {
+                range = 1;
+            }
 
             if (!client.world.getBlockState(origin).isIn(BlockTags.SHOVEL_MINEABLE)) return;
 
