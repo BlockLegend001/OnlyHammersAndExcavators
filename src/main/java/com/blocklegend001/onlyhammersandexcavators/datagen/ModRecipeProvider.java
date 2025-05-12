ab3e916de68d1924699341bc482faedab74c7fa7
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-12 12:50:38 +0200

    Changed Stone Hammer Recipe

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/datagen/ModRecipeProvider.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/datagen/ModRecipeProvider.java
@@ -140,11 +140,12 @@ public class ModRecipeProvider extends FabricRecipeProvider  {
                         .offerTo(exporter);
 
                 createShaped(RecipeCategory.TOOLS, ModItems.STONE_HAMMER)
-                        .pattern("BBB")
+                        .pattern("BCB")
                         .pattern(" S ")
                         .pattern(" S ")
                         .input('S', Items.STICK)
-                        .input('B', ItemTags.STONE_CRAFTING_MATERIALS)
+                        .input('B', Blocks.STONE)
+                        .input('C', Blocks.COBBLESTONE)
                         .criterion(hasItem(Items.STONE), conditionsFromItem(Items.COBBLESTONE))
                         .offerTo(exporter);
 
