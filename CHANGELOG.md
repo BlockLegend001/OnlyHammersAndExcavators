c0cb8564ad47260289a6c40caa38501c3b011fbb
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-12 12:48:02 +0200

    Changed Stone Hammer Recipe

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,1 @@
-- If the player is sneaking (shift held), the Excavator will only break the single targeted block.
-- Changed Recipe Registry System
-- Added Argentine Spanish Language
+- Changed Stone Hammer Recipe
--- src/main/java/com/blocklegend001/onlyhammersandexcavators/datagen/ModRecipeProvider.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/datagen/ModRecipeProvider.java
@@ -34,11 +34,12 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .offerTo(exporter);
 
         ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.STONE_HAMMER)
-                .pattern("BBB")
+                .pattern("BCB")
                 .pattern(" S ")
                 .pattern(" S ")
                 .input('S', Items.STICK)
-                .input('B', ItemTags.STONE_CRAFTING_MATERIALS)
+                .input('B', Blocks.STONE)
+                .input('C', Blocks.COBBLESTONE)
                 .criterion(hasItem(Items.STONE), conditionsFromItem(Items.COBBLESTONE))
                 .offerTo(exporter);
 
