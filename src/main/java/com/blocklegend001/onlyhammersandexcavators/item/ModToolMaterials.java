2941b2835e5e8a480089db443feb2b96861a7a0a
BlockLegend001 <rosarioarcamone@gmail.com>
2025-06-13 19:22:31 +0200

    Configurable hammer & excavator radius + tooltip display

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/item/ModToolMaterials.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/item/ModToolMaterials.java
@@ -3,10 +3,10 @@ package com.blocklegend001.onlyhammersandexcavators.item;
 import com.blocklegend001.onlyhammersandexcavators.ModConfigs;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.Tags;
@@ -15,27 +15,27 @@ import java.util.function.Supplier;
 
 public enum ModToolMaterials implements Tier {
 
-    WOODEN_HAMMER(BlockTags.INCORRECT_FOR_WOODEN_TOOL, ModConfigs.durabilityWoodenHammer, 2.0F, 0.0F, 15, () -> Ingredient.of(ItemTags.PLANKS)),
-    STONE_HAMMER(BlockTags.INCORRECT_FOR_STONE_TOOL, ModConfigs.durabilityStoneHammer, 4.0F, 1.0F, 5, () -> Ingredient.of(Tags.Items.COBBLESTONES)),
-    IRON_HAMMER(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityIronHammer, 6.0F, 2.0F, 14, () -> Ingredient.of(Items.IRON_INGOT)),
-    GOLD_HAMMER(BlockTags.INCORRECT_FOR_GOLD_TOOL, ModConfigs.durabilityGoldHammer, 12.0F, 0.0F, 22, () -> Ingredient.of(Items.GOLD_INGOT)),
-    LAPIS_HAMMER(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityLapisHammer, 6.0F, 2.0F, 14, () -> Ingredient.of(Items.LAPIS_LAZULI)),
-    REDSTONE_HAMMER(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityRedstoneHammer, 6.0F, 2.0F, 14, () -> Ingredient.of(Items.REDSTONE)),
-    DIAMOND_HAMMER(BlockTags.INCORRECT_FOR_DIAMOND_TOOL, ModConfigs.durabilityDiamondHammer, 8.0F, 3.0F, 10, () -> Ingredient.of(Items.DIAMOND)),
-    EMERALD_HAMMER(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityEmeraldHammer, 9.0F, 4.0F, 15, () -> Ingredient.of(Items.EMERALD)),
-    OBSIDIAN_HAMMER(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityObsidianHammer, 9.0F, 4.0F, 10, () -> Ingredient.of(Blocks.OBSIDIAN)),
-    NETHERITE_HAMMER(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityNetheriteHammer, 9.0F, 4.0F, 15, () -> Ingredient.of(Items.NETHERITE_INGOT)),
+    WOODEN_HAMMER(BlockTags.INCORRECT_FOR_WOODEN_TOOL, ModConfigs.durabilityWoodenHammer.get(), 2.0F, 0.0F, 15, () -> Ingredient.of(ItemTags.PLANKS)),
+    STONE_HAMMER(BlockTags.INCORRECT_FOR_STONE_TOOL, ModConfigs.durabilityStoneHammer.get(), 4.0F, 1.0F, 5, () -> Ingredient.of(Tags.Items.COBBLESTONES)),
+    IRON_HAMMER(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityIronHammer.get(), 6.0F, 2.0F, 14, () -> Ingredient.of(Items.IRON_INGOT)),
+    GOLD_HAMMER(BlockTags.INCORRECT_FOR_GOLD_TOOL, ModConfigs.durabilityGoldHammer.get(), 12.0F, 0.0F, 22, () -> Ingredient.of(Items.GOLD_INGOT)),
+    LAPIS_HAMMER(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityLapisHammer.get(), 6.0F, 2.0F, 14, () -> Ingredient.of(Items.LAPIS_LAZULI)),
+    REDSTONE_HAMMER(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityRedstoneHammer.get(), 6.0F, 2.0F, 14, () -> Ingredient.of(Items.REDSTONE)),
+    DIAMOND_HAMMER(BlockTags.INCORRECT_FOR_DIAMOND_TOOL, ModConfigs.durabilityDiamondHammer.get(), 8.0F, 3.0F, 10, () -> Ingredient.of(Items.DIAMOND)),
+    EMERALD_HAMMER(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityEmeraldHammer.get(), 9.0F, 4.0F, 15, () -> Ingredient.of(Items.EMERALD)),
+    OBSIDIAN_HAMMER(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityObsidianHammer.get(), 9.0F, 4.0F, 10, () -> Ingredient.of(Blocks.OBSIDIAN)),
+    NETHERITE_HAMMER(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityNetheriteHammer.get(), 9.0F, 4.0F, 15, () -> Ingredient.of(Items.NETHERITE_INGOT)),
 
-    WOODEN_EXCAVATOR(BlockTags.INCORRECT_FOR_WOODEN_TOOL, ModConfigs.durabilityWoodenExcavator, 2.0F, 0.0F, 15, () -> Ingredient.of(ItemTags.PLANKS)),
-    STONE_EXCAVATOR(BlockTags.INCORRECT_FOR_STONE_TOOL, ModConfigs.durabilityStoneExcavator, 4.0F, 1.0F, 5, () -> Ingredient.of(Tags.Items.COBBLESTONES)),
-    IRON_EXCAVATOR(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityIronExcavator, 6.0F, 2.0F, 14, () -> Ingredient.of(Items.IRON_INGOT)),
-    GOLD_EXCAVATOR(BlockTags.INCORRECT_FOR_GOLD_TOOL, ModConfigs.durabilityGoldExcavator, 12.0F, 0.0F, 22, () -> Ingredient.of(Items.GOLD_INGOT)),
-    LAPIS_EXCAVATOR(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityLapisExcavator, 6.0F, 2.0F, 14, () -> Ingredient.of(Items.LAPIS_LAZULI)),
-    REDSTONE_EXCAVATOR(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityRedstoneExcavator, 6.0F, 2.0F, 14, () -> Ingredient.of(Items.REDSTONE)),
-    DIAMOND_EXCAVATOR(BlockTags.INCORRECT_FOR_DIAMOND_TOOL, ModConfigs.durabilityDiamondExcavator, 8.0F, 3.0F, 10, () -> Ingredient.of(Items.DIAMOND)),
-    EMERALD_EXCAVATOR(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityEmeraldExcavator, 9.0F, 4.0F, 15, () -> Ingredient.of(Items.EMERALD)),
-    OBSIDIAN_EXCAVATOR(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityObsidianExcavator, 9.0F, 4.0F, 10, () -> Ingredient.of(Blocks.OBSIDIAN)),
-    NETHERITE_EXCAVATOR(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityNetheriteExcavator, 9.0F, 4.0F, 15, () -> Ingredient.of(Items.NETHERITE_INGOT));
+    WOODEN_EXCAVATOR(BlockTags.INCORRECT_FOR_WOODEN_TOOL, ModConfigs.durabilityWoodenExcavator.get(), 2.0F, 0.0F, 15, () -> Ingredient.of(ItemTags.PLANKS)),
+    STONE_EXCAVATOR(BlockTags.INCORRECT_FOR_STONE_TOOL, ModConfigs.durabilityStoneExcavator.get(), 4.0F, 1.0F, 5, () -> Ingredient.of(Tags.Items.COBBLESTONES)),
+    IRON_EXCAVATOR(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityIronExcavator.get(), 6.0F, 2.0F, 14, () -> Ingredient.of(Items.IRON_INGOT)),
+    GOLD_EXCAVATOR(BlockTags.INCORRECT_FOR_GOLD_TOOL, ModConfigs.durabilityGoldExcavator.get(), 12.0F, 0.0F, 22, () -> Ingredient.of(Items.GOLD_INGOT)),
+    LAPIS_EXCAVATOR(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityLapisExcavator.get(), 6.0F, 2.0F, 14, () -> Ingredient.of(Items.LAPIS_LAZULI)),
+    REDSTONE_EXCAVATOR(BlockTags.INCORRECT_FOR_IRON_TOOL, ModConfigs.durabilityRedstoneExcavator.get(), 6.0F, 2.0F, 14, () -> Ingredient.of(Items.REDSTONE)),
+    DIAMOND_EXCAVATOR(BlockTags.INCORRECT_FOR_DIAMOND_TOOL, ModConfigs.durabilityDiamondExcavator.get(), 8.0F, 3.0F, 10, () -> Ingredient.of(Items.DIAMOND)),
+    EMERALD_EXCAVATOR(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityEmeraldExcavator.get(), 9.0F, 4.0F, 15, () -> Ingredient.of(Items.EMERALD)),
+    OBSIDIAN_EXCAVATOR(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityObsidianExcavator.get(), 9.0F, 4.0F, 10, () -> Ingredient.of(Blocks.OBSIDIAN)),
+    NETHERITE_EXCAVATOR(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, ModConfigs.durabilityNetheriteExcavator.get(), 9.0F, 4.0F, 15, () -> Ingredient.of(Items.NETHERITE_INGOT));
 
 
     private final TagKey<Block> incorrect;
