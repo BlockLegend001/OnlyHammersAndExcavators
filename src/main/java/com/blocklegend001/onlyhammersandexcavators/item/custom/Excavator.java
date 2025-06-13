bc47af135786c1a8deb5f4a6a3b6866f96c00c76
BlockLegend001 <rosarioarcamone@gmail.com>
2025-06-13 12:07:08 +0200

    Configurable excavator & hammer radius + tooltip display

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/item/custom/Excavator.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/item/custom/Excavator.java
@@ -1,8 +1,11 @@
 package com.blocklegend001.onlyhammersandexcavators.item.custom;
 
 import net.minecraft.item.*;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
@@ -10,6 +13,8 @@ import net.minecraft.util.math.Direction;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.blocklegend001.onlyhammersandexcavators.utils.RadiusMap.EXCAVATOR_RADIUS_MAP;
 
 public class Excavator extends MiningToolItem {
     public Excavator(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
@@ -53,5 +58,26 @@ public class Excavator extends MiningToolItem {
         }
 
         return positions;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        int radius = getRadiusForExcavator(stack);
+
+        Text text = Text.empty()
+                .append(Text.literal("Dig Radius: ").formatted(Formatting.GRAY))
+                .append(Text.literal(String.valueOf(radius)).formatted(Formatting.YELLOW))
+                .append(Text.literal(" Blocks").formatted(Formatting.GRAY));
+
+        tooltip.add(text);
+
+        super.appendTooltip(stack, context, tooltip, type);
+    }
+
+    private int getRadiusForExcavator(ItemStack stack) {
+        if (EXCAVATOR_RADIUS_MAP.containsKey(stack.getItem())) {
+            return EXCAVATOR_RADIUS_MAP.get(stack.getItem());
+        }
+        return 0;
     }
 }
