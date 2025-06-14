f4212f30784a24876dcea0a667aa485dd4f6aab9
BlockLegend001 <rosarioarcamone@gmail.com>
2025-06-14 10:14:40 +0200

    Configurable hammer & excavator radius + tooltip display

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/item/custom/Hammer.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/item/custom/Hammer.java
@@ -1,12 +1,18 @@
 package com.blocklegend001.onlyhammersandexcavators.item.custom;
 
+import com.blocklegend001.onlyhammersandexcavators.utils.RadiusMap;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ToolMaterial;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.phys.BlockHitResult;
@@ -14,6 +20,7 @@ import net.minecraft.world.phys.HitResult;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Hammer extends Item {
     private static TagKey<Block> pickaxeMineable;
@@ -62,6 +69,27 @@ public class Hammer extends Item {
             }
         }
         return positions;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, TooltipDisplay tooltip, Consumer<Component> consumer, TooltipFlag type) {
+        int radius = getRadiusForExcavator(stack);
+
+        Component text = Component.empty()
+                .append(Component.literal("Dig Radius: ").withStyle(ChatFormatting.GRAY))
+                .append(Component.literal(String.valueOf(radius)).withStyle(ChatFormatting.YELLOW))
+                .append(Component.literal(" Blocks").withStyle(ChatFormatting.GRAY));
+
+        consumer.accept(text);
+
+        super.appendHoverText(stack, context, tooltip, consumer, type);
+    }
+
+    private int getRadiusForExcavator(ItemStack stack) {
+        if (RadiusMap.getHammerRadius().containsKey(stack.getItem())) {
+            return RadiusMap.getHammerRadius().get(stack.getItem());
+        }
+        return 0;
     }
 
     private static ToolMaterial wrapMaterial(ToolMaterial toolMaterial, int durability) {
