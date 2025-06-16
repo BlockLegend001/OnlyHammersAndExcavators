257650553491e21f193f32a711e66652031c489f
BlockLegend001 <rosarioarcamone@gmail.com>
2025-06-16 18:50:45 +0200

    Improve tooltip by showing hammering & excavation area dimensions instead of radius

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/item/custom/Excavator.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/item/custom/Excavator.java
@@ -58,11 +58,11 @@ public class Excavator extends MiningToolItem {
     @Override
     public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
         int radius = getRadiusForExcavator(stack);
+        int widht = radius * 2 + 1;
 
-        Text text = Text.empty()
-                .append(Text.literal("Dig Radius: ").formatted(Formatting.GRAY))
-                .append(Text.literal(String.valueOf(radius)).formatted(Formatting.YELLOW))
-                .append(Text.literal(" Blocks").formatted(Formatting.GRAY));
+        Text text = Text.literal("Dig area: ")
+                .formatted(Formatting.GRAY)
+                .append(Text.literal(widht + "x1").formatted(Formatting.YELLOW));
 
         tooltip.add(text);
 
