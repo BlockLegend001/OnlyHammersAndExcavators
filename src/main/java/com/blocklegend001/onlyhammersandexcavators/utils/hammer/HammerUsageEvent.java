145b6843ddb086fd04d942e7dc4da7c74dcb2021
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-05 12:52:20 +0200

    Added 1.21.7-Fabric

---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/hammer/HammerUsageEvent.java
@@ -0,0 +1,54 @@
+package com.blocklegend001.onlyhammersandexcavators.utils.hammer;
+
+import com.blocklegend001.onlyhammersandexcavators.item.custom.Hammer;
+import com.blocklegend001.onlyhammersandexcavators.utils.RadiusMap;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.blocklegend001.onlyhammersandexcavators.item.custom.Hammer.getBlocksToBeDestroyed;
+
+public class HammerUsageEvent implements PlayerBlockBreakEvents.Before{
+    private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>();
+    public static boolean isSneaking = false;
+
+    @Override
+    public boolean beforeBlockBreak(World world, PlayerEntity player, BlockPos pos,
+                                    BlockState state, @Nullable BlockEntity blockEntity) {
+        if (!(player instanceof ServerPlayerEntity serverPlayer)) return true;
+
+        ItemStack mainHandItem = player.getMainHandStack();
+        if (!(mainHandItem.getItem() instanceof Hammer excavator)) return true;
+
+        if (HARVESTED_BLOCKS.contains(pos)) return true;
+
+        HARVESTED_BLOCKS.add(pos);
+
+        try {
+            int radius = isSneaking ? 0 : RadiusMap.HAMMER_RADIUS_MAP.get(mainHandItem.getItem());
+            for (BlockPos targetPos : getBlocksToBeDestroyed(radius, pos, serverPlayer)) {
+                if (targetPos.equals(pos)) continue;
+
+                if (HARVESTED_BLOCKS.contains(targetPos)) continue;
+                if (!excavator.isCorrectForDrops(mainHandItem, world.getBlockState(targetPos))) continue;
+
+                HARVESTED_BLOCKS.add(targetPos);
+                serverPlayer.interactionManager.tryBreakBlock(targetPos);
+                HARVESTED_BLOCKS.remove(targetPos);
+            }
+        } finally {
+            HARVESTED_BLOCKS.remove(pos);
+        }
+
+        return true;
+    }
+}
