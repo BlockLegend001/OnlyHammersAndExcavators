f09293808ff3014c9563596ab623b793a1037d05
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-06 18:33:23 +0200

    Fixed a bug that caused the server to crash

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/OverlayRenderer.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/OverlayRenderer.java
@@ -6,7 +6,6 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.world.phys.AABB;
 import org.joml.Matrix4f;
 
-@Mod.EventBusSubscriber(modid = OnlyHammersAndExcavators.MOD_ID)
 public class OverlayRenderer {
 
     public static void drawBox(Matrix4f matrix, VertexConsumer buffer, AABB box, float r, float g, float b, float a) {
