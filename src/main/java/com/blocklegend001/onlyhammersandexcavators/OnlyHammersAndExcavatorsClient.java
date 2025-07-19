9564628d7cb04322ea66a1fc64ecea210fa80848
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-19 12:55:39 +0200

    Fixed a bug that not let the player break one block when shifting in multiplayer

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/OnlyHammersAndExcavatorsClient.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/OnlyHammersAndExcavatorsClient.java
@@ -1,24 +1,13 @@
 package com.blocklegend001.onlyhammersandexcavators;
 
 import com.blocklegend001.onlyhammersandexcavators.utils.excavator.ExcavatorOverlayRenderer;
-import com.blocklegend001.onlyhammersandexcavators.utils.excavator.ExcavatorUsageEvent;
 import com.blocklegend001.onlyhammersandexcavators.utils.hammer.HammerOverlayRenderer;
-import com.blocklegend001.onlyhammersandexcavators.utils.hammer.HammerUsageEvent;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
-import net.minecraft.util.ActionResult;
 
 public class OnlyHammersAndExcavatorsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ExcavatorOverlayRenderer.init();
         HammerOverlayRenderer.init();
-        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
-            if (world.isClient) {
-                HammerUsageEvent.isSneaking = player.isSneaking();
-                ExcavatorUsageEvent.isSneaking = player.isSneaking();
-            }
-            return ActionResult.PASS;
-        });
     }
 }
