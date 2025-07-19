6681e22ae439ae99c41ceaa8bc2928604635f1e5
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-19 12:16:25 +0200

    Fixed a bug that not let the player break one block when shifting in multiplayer

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/event/ModEventsExcavator.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/event/ModEventsExcavator.java
@@ -16,27 +16,27 @@ import java.util.Set;
 
 @EventBusSubscriber(modid = OnlyHammersAndExcavators.MODID)
 public class ModEventsExcavator {
+
     private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>();
-    public static boolean isSneaking = false;
 
     @SubscribeEvent
-    public static boolean onExcavatorUsage(BlockEvent.BreakEvent event) {
+    public static void onExcavatorUsage(BlockEvent.BreakEvent event) {
         Player player = event.getPlayer();
         ItemStack mainHandItem = player.getMainHandItem();
 
-        if (!(player instanceof ServerPlayer serverPlayer)) return true;
+        if (!(player instanceof ServerPlayer serverPlayer)) return;
+        if (!(mainHandItem.getItem() instanceof Excavator excavator)) return;
+        if (HARVESTED_BLOCKS.contains(event.getPos())) return;
 
-        if (!(mainHandItem.getItem() instanceof Excavator excavator)) return true;
-
-        if (HARVESTED_BLOCKS.contains(event.getPos())) return true;
+        boolean isSneaking = player.isCrouching();
 
         HARVESTED_BLOCKS.add(event.getPos());
 
         try {
             int radius = isSneaking ? 0 : RadiusMap.getExcavatorRadius().get(mainHandItem.getItem());
+
             for (BlockPos targetPos : Excavator.getBlocksToBeDestroyed(radius, event.getPos(), serverPlayer)) {
                 if (targetPos.equals(event.getPos())) continue;
-
                 if (HARVESTED_BLOCKS.contains(targetPos)) continue;
                 if (!excavator.isCorrectToolForDrops(mainHandItem, event.getLevel().getBlockState(targetPos))) continue;
 
@@ -44,10 +44,9 @@ public class ModEventsExcavator {
                 serverPlayer.gameMode.destroyBlock(targetPos);
                 HARVESTED_BLOCKS.remove(targetPos);
             }
+
         } finally {
             HARVESTED_BLOCKS.remove(event.getPos());
         }
-
-        return true;
     }
 }
