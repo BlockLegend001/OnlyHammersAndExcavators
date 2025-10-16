7076a6dab9909e18ad7d3a82cf9116e8ea2ac929
BlockLegend001 <rosarioarcamone@gmail.com>
2025-10-16 12:13:47 +0200

    Added 1.21.10-Forge

---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/OnlyHammersAndExcavators.java
@@ -0,0 +1,62 @@
+package com.blocklegend001.onlyhammersandexcavators;
+
+import com.blocklegend001.onlyhammersandexcavators.item.ModCreativeModeTabs;
+import com.blocklegend001.onlyhammersandexcavators.item.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
+import org.slf4j.Logger;
+
+@Mod(OnlyHammersAndExcavators.MOD_ID)
+public class OnlyHammersAndExcavators {
+
+    public static final String MOD_ID = "onlyhammersandexcavators";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public OnlyHammersAndExcavators(FMLJavaModLoadingContext context) {
+        var modEventBus = context.getModBusGroup();
+        context.registerConfig(ModConfig.Type.COMMON, ModConfigs.SPEC, "onlyhammersandexcavators.toml");
+        ModConfigs.loadConfig(ModConfigs.SPEC, FMLPaths.CONFIGDIR.get().resolve("onlyhammersandexcavators.toml"));
+        ModCreativeModeTabs.register(modEventBus);
+        ModItems.register(modEventBus);
+        BuildCreativeModeTabContentsEvent.BUS.addListener(this::addCreative);
+        FMLCommonSetupEvent.getBus(modEventBus).addListener(this::setup);
+    }
+
+    @SubscribeEvent
+    public void setup(final FMLCommonSetupEvent event) {
+        ModConfigs.loadConfig(ModConfigs.SPEC, FMLPaths.CONFIGDIR.get().resolve("onlyhammersandexcavators.toml"));
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTab() == ModCreativeModeTabs.ONLYHAMMERSANDEXCAVATORS.get()) {
+            event.accept(ModItems.WOODEN_EXCAVATOR.get());
+            event.accept(ModItems.STONE_EXCAVATOR.get());
+            event.accept(ModItems.IRON_EXCAVATOR.get());
+            event.accept(ModItems.GOLD_EXCAVATOR.get());
+            event.accept(ModItems.LAPIS_EXCAVATOR.get());
+            event.accept(ModItems.REDSTONE_EXCAVATOR.get());
+            event.accept(ModItems.DIAMOND_EXCAVATOR.get());
+            event.accept(ModItems.OBSIDIAN_EXCAVATOR.get());
+            event.accept(ModItems.NETHERITE_EXCAVATOR.get());
+
+            event.accept(ModItems.EMERALD_EXCAVATOR.get());
+            event.accept(ModItems.WOODEN_HAMMER.get());
+            event.accept(ModItems.STONE_HAMMER.get());
+            event.accept(ModItems.IRON_HAMMER.get());
+            event.accept(ModItems.GOLD_HAMMER.get());
+            event.accept(ModItems.LAPIS_HAMMER.get());
+            event.accept(ModItems.REDSTONE_HAMMER.get());
+            event.accept(ModItems.DIAMOND_HAMMER.get());
+            event.accept(ModItems.EMERALD_HAMMER.get());
+            event.accept(ModItems.OBSIDIAN_HAMMER.get());
+            event.accept(ModItems.NETHERITE_HAMMER.get());
+        }
+    }
+}
---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/item/custom/Hammer.java
@@ -0,0 +1,90 @@
+package com.blocklegend001.onlyhammersandexcavators.item.custom;
+
+import com.blocklegend001.onlyhammersandexcavators.item.ModToolMaterials;
+import com.blocklegend001.onlyhammersandexcavators.utils.RadiusMap;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ToolMaterial;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Hammer extends Item {
+
+    public Hammer(ModToolMaterials material, float attackDamage, float attackSpeed, Properties settings) {
+        super(material.applyToolProperties(settings, BlockTags.MINEABLE_WITH_PICKAXE, attackDamage, attackSpeed));
+    }
+
+    public static List<BlockPos> getBlocksToBeDestroyed(int range, BlockPos initalBlockPos, ServerPlayer player) {
+        List<BlockPos> positions = new ArrayList<>();
+
+        BlockHitResult traceResult = player.level().clip(new ClipContext(player.getEyePosition(1f),
+                (player.getEyePosition(1f).add(player.getViewVector(1f).scale(6f))),
+                ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, player));
+        if(traceResult.getType() == HitResult.Type.MISS) {
+            return positions;
+        }
+
+        if (traceResult.getDirection() == Direction.DOWN || traceResult.getDirection() == Direction.UP) {
+            for (int x = -range; x <= range; x++) {
+                for (int y = -range; y <= range; y++) {
+                    positions.add(new BlockPos(initalBlockPos.getX() + x, initalBlockPos.getY(), initalBlockPos.getZ() + y));
+                }
+            }
+        }
+
+        if(traceResult.getDirection() == Direction.NORTH || traceResult.getDirection() == Direction.SOUTH) {
+            for(int x = -range; x <= range; x++) {
+                for(int y = -range; y <= range; y++) {
+                    positions.add(new BlockPos(initalBlockPos.getX() + x, initalBlockPos.getY() + y, initalBlockPos.getZ()));
+                }
+            }
+        }
+
+        if(traceResult.getDirection() == Direction.EAST || traceResult.getDirection() == Direction.WEST) {
+            for(int x = -range; x <= range; x++) {
+                for(int y = -range; y <= range; y++) {
+                    positions.add(new BlockPos(initalBlockPos.getX(), initalBlockPos.getY() + y, initalBlockPos.getZ() + x));
+                }
+            }
+        }
+
+        return positions;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, TooltipDisplay tooltip, Consumer<Component> consumer, TooltipFlag type) {
+        int radius = getRadiusForHammer(stack);
+        int widht = radius * 2 + 1;
+
+        Component text = Component.literal("Dig area: ")
+                .withStyle(ChatFormatting.GRAY)
+                .append(Component.literal(widht + "x1").withStyle(ChatFormatting.YELLOW));
+
+        consumer.accept(text);
+
+        super.appendHoverText(stack, context, tooltip, consumer, type);
+    }
+
+    private int getRadiusForHammer(ItemStack stack) {
+        if (RadiusMap.getHammerRadius().containsKey(stack.getItem())) {
+            return RadiusMap.getHammerRadius().get(stack.getItem());
+        }
+        return 0;
+    }
+}
+
