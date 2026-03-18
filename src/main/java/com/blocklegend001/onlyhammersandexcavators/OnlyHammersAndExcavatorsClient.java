5875d10fbb87ba4b66e420038a2460dbda461d6d
BlockLegend001 <rosarioarcamone@gmail.com>
2026-03-18 20:26:02 +0100

    Added KeyBind to Enable/Disable the Outline

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/OnlyHammersAndExcavatorsClient.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/OnlyHammersAndExcavatorsClient.java
@@ -3,11 +3,47 @@ package com.blocklegend001.onlyhammersandexcavators;
 import com.blocklegend001.onlyhammersandexcavators.utils.excavator.ExcavatorOverlayRenderer;
 import com.blocklegend001.onlyhammersandexcavators.utils.hammer.HammerOverlayRenderer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
 public class OnlyHammersAndExcavatorsClient implements ClientModInitializer {
+    public static KeyBinding SHOW_OUTLINE_KEY;
+    private static boolean wasPressed = false;
+    public static boolean SHOW_OUTLINE_ENABLED = true;
+
     @Override
     public void onInitializeClient() {
-        ExcavatorOverlayRenderer.init();
+        SHOW_OUTLINE_KEY = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key.onlyhammersandexcavators.showoutline",
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_O,
+                        "key.category.onlyhammersandexcavators.showoutline"
+                )
+        );
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (SHOW_OUTLINE_KEY.isPressed()) {
+                if (!wasPressed) {
+                    SHOW_OUTLINE_ENABLED = !SHOW_OUTLINE_ENABLED;
+
+                    if (client.player != null) {
+                        client.player.sendMessage(
+                                Text.literal("Outline: " + (SHOW_OUTLINE_ENABLED ? "ON" : "OFF")),
+                                false
+                        );
+                    }
+                }
+                wasPressed = true;
+            } else {
+                wasPressed = false;
+            }
+        });
         HammerOverlayRenderer.init();
+        ExcavatorOverlayRenderer.init();
     }
 }
