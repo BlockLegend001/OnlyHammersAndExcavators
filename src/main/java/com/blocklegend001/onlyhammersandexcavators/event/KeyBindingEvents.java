d9a8c68c460496df91af57e78e95162274307bac
BlockLegend001 <rosarioarcamone@gmail.com>
2026-04-07 21:06:03 +0200

    Added KeyBind to Enable/Disable the Outline

---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/event/KeyBindingEvents.java
@@ -0,0 +1,35 @@
+package com.blocklegend001.onlyhammersandexcavators.event;
+
+import com.blocklegend001.onlyhammersandexcavators.OnlyHammersAndExcavators;
+import com.blocklegend001.onlyhammersandexcavators.utils.KeyBinding;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+
+@EventBusSubscriber(modid = OnlyHammersAndExcavators.MODID, value = Dist.CLIENT)
+public class KeyBindingEvents {
+
+    @SubscribeEvent
+    public static void onClientTick(ClientTickEvent.Post event) {
+
+        if (KeyBinding.SHOW_OUTLINE_KEY.consumeClick()) {
+
+            OnlyHammersAndExcavators.SHOW_OUTLINE_ENABLED =
+                    !OnlyHammersAndExcavators.SHOW_OUTLINE_ENABLED;
+
+            var mc = Minecraft.getInstance();
+
+            if (mc.player != null) {
+                mc.player.displayClientMessage(
+                        Component.literal(
+                                "Outline: " +
+                                        (OnlyHammersAndExcavators.SHOW_OUTLINE_ENABLED ? "ON" : "OFF")
+                        ), false
+                );
+            }
+        }
+    }
+}
