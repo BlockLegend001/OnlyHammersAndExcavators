da2b5d17352a2d497bce50065c19c0417ed6b454
BlockLegend001 <rosarioarcamone@gmail.com>
2026-04-29 13:58:39 +0200

    Bug Fixes

---
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/event/RenderEvent.java
@@ -0,0 +1,105 @@
+package com.blocklegend001.onlyhammersandexcavators.event;
+
+import com.blocklegend001.onlyhammersandexcavators.OnlyHammersAndExcavators;
+import com.blocklegend001.onlyhammersandexcavators.item.custom.Hammer;
+import com.blocklegend001.onlyhammersandexcavators.item.custom.Excavator;
+import com.blocklegend001.onlyhammersandexcavators.utils.OverlayRenderer;
+import com.blocklegend001.onlyhammersandexcavators.utils.RadiusMap;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderLevelStageEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.joml.Matrix4f;
+
+@Mod.EventBusSubscriber(modid = OnlyHammersAndExcavators.MOD_ID, value = Dist.CLIENT)
+public class RenderEvent {
+
+    @SubscribeEvent
+    public static void onRenderLevel(RenderLevelStageEvent event) {
+        if (event.getStage() != RenderLevelStageEvent.Stage.AFTER_TRANSLUCENT_BLOCKS) return;
+
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.level == null || mc.player == null) return;
+        if (!OnlyHammersAndExcavators.SHOW_OUTLINE_ENABLED) return;
+
+        ItemStack heldItem = mc.player.getMainHandItem();
+
+        boolean isHammer = heldItem.getItem() instanceof Hammer;
+        boolean isExcavator = heldItem.getItem() instanceof Excavator;
+
+        if (!isHammer && !isExcavator) return;
+
+        if (!(mc.hitResult instanceof BlockHitResult blockHit) ||
+                blockHit.getType() != HitResult.Type.BLOCK) return;
+
+        BlockPos origin = blockHit.getBlockPos();
+        Direction side = blockHit.getDirection();
+
+        if (isHammer && !mc.level.getBlockState(origin).is(BlockTags.MINEABLE_WITH_PICKAXE)) return;
+        if (isExcavator && !mc.level.getBlockState(origin).is(BlockTags.MINEABLE_WITH_SHOVEL)) return;
+
+        int range = 0;
+
+        if (!mc.player.isShiftKeyDown()) {
+            if (isHammer) {
+                range = RadiusMap.getHammerRadius().getOrDefault(heldItem.getItem(), 0);
+            } else if (isExcavator) {
+                range = RadiusMap.getExcavatorRadius().getOrDefault(heldItem.getItem(), 0);
+            }
+        }
+
+        AABB box = getSelectionBox(range, side, origin);
+
+        Camera camera = event.getCamera();
+        Vec3 camPos = camera.getPosition();
+
+        Matrix4f viewMatrix = new Matrix4f();
+        viewMatrix.identity();
+        viewMatrix.translate((float) -camPos.x, (float) -camPos.y, (float) -camPos.z);
+
+        Matrix4f combinedMatrix = new Matrix4f(event.getProjectionMatrix());
+        combinedMatrix.mul(viewMatrix);
+
+        MultiBufferSource.BufferSource buffer = mc.renderBuffers().bufferSource();
+        VertexConsumer builder = buffer.getBuffer(RenderType.lines());
+
+        OverlayRenderer.drawBox(builder, camera.getPosition(), box, 1f, 1f, 1f, 1f);
+
+        buffer.endBatch();
+    }
+
+    private static AABB getSelectionBox(int range, Direction side, BlockPos origin) {
+        int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE, minZ = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE, maxZ = Integer.MIN_VALUE;
+
+        for (int x = -range; x <= range; x++) {
+            for (int y = -range; y <= range; y++) {
+                BlockPos pos = switch (side.getAxis()) {
+                    case Y -> origin.offset(x, 0, y);
+                    case X -> origin.offset(0, y, x);
+                    case Z -> origin.offset(x, y, 0);
+                };
+                minX = Math.min(minX, pos.getX());
+                minY = Math.min(minY, pos.getY());
+                minZ = Math.min(minZ, pos.getZ());
+                maxX = Math.max(maxX, pos.getX() + 1);
+                maxY = Math.max(maxY, pos.getY() + 1);
+                maxZ = Math.max(maxZ, pos.getZ() + 1);
+            }
+        }
+        return new AABB(minX, minY, minZ, maxX, maxY, maxZ);
+    }
+}
--- src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/OverlayRenderer.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/OverlayRenderer.java
@@ -2,22 +2,14 @@ package com.blocklegend001.onlyhammersandexcavators.utils;
 
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 
 public class OverlayRenderer {
 
-    public static void drawBox(Matrix4f matrix, VertexConsumer buffer, AABB box, float r, float g, float b, float a) {
-        float minX = (float) box.minX;
-        float minY = (float) box.minY;
-        float minZ = (float) box.minZ;
-        float maxX = (float) box.maxX;
-        float maxY = (float) box.maxY;
-        float maxZ = (float) box.maxZ;
-
-        float[][] corners = {
-                {minX, minY, minZ}, {maxX, minY, minZ}, {minX, maxY, minZ}, {maxX, maxY, minZ},
-                {minX, minY, maxZ}, {maxX, minY, maxZ}, {minX, maxY, maxZ}, {maxX, maxY, maxZ}
-        };
+    public static void drawBox(VertexConsumer buffer, Vec3 camPos, AABB box, float r, float g, float b, float a) {
+        float[][] corners = getFloats(camPos, box);
 
         int[][] edges = {
                 {0,1},{1,3},{3,2},{2,0},
@@ -28,8 +20,23 @@ public class OverlayRenderer {
         for (int[] edge : edges) {
             float[] p1 = corners[edge[0]];
             float[] p2 = corners[edge[1]];
-            buffer.addVertex(matrix, p1[0], p1[1], p1[2]).setColor(r, g, b, a).setNormal(1, 0, 0);
-            buffer.addVertex(matrix, p2[0], p2[1], p2[2]).setColor(r, g, b, a).setNormal(1, 0, 0);
+            buffer.addVertex(p1[0], p1[1], p1[2]).setColor(r, g, b, a).setNormal(1, 0, 0);
+            buffer.addVertex(p2[0], p2[1], p2[2]).setColor(r, g, b, a).setNormal(1, 0, 0);
         }
+    }
+
+    private static float[] @NotNull [] getFloats(Vec3 camPos, AABB box) {
+        float minX = (float) (box.minX - camPos.x);
+        float minY = (float) (box.minY - camPos.y);
+        float minZ = (float) (box.minZ - camPos.z);
+        float maxX = (float) (box.maxX - camPos.x);
+        float maxY = (float) (box.maxY - camPos.y);
+        float maxZ = (float) (box.maxZ - camPos.z);
+
+        float[][] corners = {
+                {minX, minY, minZ}, {maxX, minY, minZ}, {minX, maxY, minZ}, {maxX, maxY, minZ},
+                {minX, minY, maxZ}, {maxX, minY, maxZ}, {minX, maxY, maxZ}, {maxX, maxY, maxZ}
+        };
+        return corners;
     }
 }
