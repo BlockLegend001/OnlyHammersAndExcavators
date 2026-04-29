c3b893a4256488cd02c50596a6c5a0e8dbf11661
BlockLegend001 <rosarioarcamone@gmail.com>
2026-04-29 23:03:44 +0200

    Bug Fixes

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/OnlyHammersAndExcavators.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/OnlyHammersAndExcavators.java
@@ -2,7 +2,9 @@ package com.blocklegend001.onlyhammersandexcavators;
 
 import com.blocklegend001.onlyhammersandexcavators.item.ModCreativeModeTabs;
 import com.blocklegend001.onlyhammersandexcavators.item.ModItems;
+import com.blocklegend001.onlyhammersandexcavators.utils.KeyBinding;
 import com.mojang.logging.LogUtils;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
@@ -28,6 +30,8 @@ public class OnlyHammersAndExcavators {
         ModItems.register(modEventBus);
         BuildCreativeModeTabContentsEvent.BUS.addListener(this::addCreative);
         FMLCommonSetupEvent.getBus(modEventBus).addListener(this::setup);
+
+        RegisterKeyMappingsEvent.BUS.addListener(KeyBinding::registerKeys);
     }
 
     @SubscribeEvent
