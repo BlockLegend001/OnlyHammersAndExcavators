96d85d1176ff9d2ad1812b3f7349ce829c43b274
BlockLegend001 <rosarioarcamone@gmail.com>
2026-04-30 11:47:07 +0200

    Bug Fixes

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/KeyBinding.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/KeyBinding.java
@@ -13,7 +13,7 @@ import org.lwjgl.glfw.GLFW;
 
 public class KeyBinding {
 
-    public static final KeyMapping SHOW_OUTLINE_KEY = new KeyMapping(
+    public static final KeyMapping HAE_SHOW_OUTLINE_KEY = new KeyMapping(
             "key.onlyhammersandexcavators.showoutline",
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_O,
@@ -22,6 +22,6 @@ public class KeyBinding {
 
     @SubscribeEvent
     public static void registerKeys(RegisterKeyMappingsEvent event) {
-        event.register(SHOW_OUTLINE_KEY);
+        event.register(HAE_SHOW_OUTLINE_KEY);
     }
 }
