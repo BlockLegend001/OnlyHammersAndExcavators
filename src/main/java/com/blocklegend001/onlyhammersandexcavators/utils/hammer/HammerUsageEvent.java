7c137010308eb8733a094d394a71590103af79e1
BlockLegend001 <rosarioarcamone@gmail.com>
2026-09-07 19:15:52 +0200

    Small Bug Fix

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/hammer/HammerUsageEvent.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/utils/hammer/HammerUsageEvent.java
@@ -26,7 +26,8 @@ public class HammerUsageEvent implements PlayerBlockBreakEvents.Before{
         if (!(player instanceof ServerPlayerEntity serverPlayer)) return true;
 
         ItemStack mainHandItem = player.getMainHandStack();
-        if (!(mainHandItem.getItem() instanceof Hammer excavator)) return true;
+        if (!(mainHandItem.getItem() instanceof Hammer hammer)) return true;
+        if (state.getHardness(world, pos) == 0.0F)  return true;
 
         if (HARVESTED_BLOCKS.contains(pos)) return true;
 
@@ -40,7 +41,7 @@ public class HammerUsageEvent implements PlayerBlockBreakEvents.Before{
                 if (targetPos.equals(pos)) continue;
 
                 if (HARVESTED_BLOCKS.contains(targetPos)) continue;
-                if (!excavator.isCorrectForDrops(mainHandItem, world.getBlockState(targetPos))) continue;
+                if (!hammer.isCorrectForDrops(mainHandItem, world.getBlockState(targetPos))) continue;
 
                 HARVESTED_BLOCKS.add(targetPos);
                 serverPlayer.interactionManager.tryBreakBlock(targetPos);
