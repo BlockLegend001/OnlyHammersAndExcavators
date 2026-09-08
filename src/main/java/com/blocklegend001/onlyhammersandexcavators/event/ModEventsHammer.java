16281dcf6c2bba36a70888bb8540c5d50a25ba79
BlockLegend001 <rosarioarcamone@gmail.com>
2026-09-08 11:38:33 +0200

    Small bug fix

--- src/main/java/com/blocklegend001/onlyhammersandexcavators/event/ModEventsHammer.java
+++ src/main/java/com/blocklegend001/onlyhammersandexcavators/event/ModEventsHammer.java
@@ -25,6 +25,8 @@ public class ModEventsHammer {
 
         if (!(player instanceof ServerPlayer serverPlayer)) return true;
         if (!(mainHandItem.getItem() instanceof Hammer hammer)) return true;
+        if (event.getState().getDestroySpeed(event.getLevel(), event.getPos()) == 0.0F) return true;
+
         if (HARVESTED_BLOCKS.contains(event.getPos())) return true;
 
         boolean isSneaking = player.isCrouching() || player.isShiftKeyDown();
